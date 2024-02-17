e46fe6f7cb4190550b11354fefb545142640a435
Obie Carnathan <aikia16@uab.edu>
2024-02-17 14:17:12 -0600

    Program now successfully finds all photos and videos. Next step is to start organizing photos and videos into new folders by type.

--- photoOrganizer.py
+++ photoOrganizer.py
@@ -9,33 +9,80 @@ print(
 # specifying the path to my external SSD drive. Given by user.
 # ssd_Path = input()  # /Volumes/Sandisk SSD
 
+photo_extensions = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".tiff",
+    ".tif",
+    ".nef",
+    ".cr2",
+    ".arw",
+    ".heic",
+]
+
+video_extensions = [
+    ".mp4",
+    ".avi",
+    ".mkv",
+    ".mov",
+    ".wmv",
+    ".flv",
+    ".mpeg",
+    ".mpg",
+    ".webm",
+    ".3gp",
+    ".asf",
+]
+
 
 def printFolder(path):
-    if os.path.exists(path):
-        files = os.listdir(path)
-        count = 0
-        for file in files:
-            file_path = os.path.join(path, file)
-            count = count + 1
-            if os.path.isdir(file_path):
-                print(f"{file} is a directory.")
-                printFolder(file)
-            else:
-                print(f"{file} is not a directory.")
-        print("\nAll files printed")
-        print(count)
+    try:
+        if os.path.exists(path):  # if the file path is an actual path
+            files = os.listdir(
+                path
+            )  # creates a list of files/folders in the current directory
+            count = 0  # used to keep track of the total files
+            for file in files:  # Loops through the list of files
+                file_path = os.path.join(
+                    path, file
+                )  # Creates a file path by joining the path with the current file name
+                try:
+                    if os.path.isdir(
+                        file_path
+                    ):  # Checks to see if the current file is actually a folder
+                        # print(f"{file} is a directory.")  # prints the folder name
+                        # print("\n")
+                        printFolder(
+                            file_path
+                        )  # recursively uses the current folder to print the files that it contains
+                    else:
+                        # print(f"{file} is not a directory.")  # prints the file
+                        file_extension = os.path.splitext(file)[1]
+                        if str(file_extension).lower() in (
+                            photo_extensions or video_extensions
+                        ):
+                            print(file)
+                            if file_extension.lower() == ".heic":
+                                print("iphone")
+                                break
 
-    else:
-        print(f"path {path} does not exist.")
+                except (
+                    PermissionError
+                ) as e:  # Some files are restricted so we need this except case
+                    print(f"PermissionError: {e}. Skipping {file_path}")
+
+            # print("\nAll files printed")
+            # print(count)
+
+        else:
+            print(f"path {path} does not exist.")
+    except (
+        PermissionError
+    ) as e:  # Some files are restricted so we need this except case
+        print(f"PermissionError: {e}. Skipping {path}")
 
 
 printFolder("/Volumes/Sandisk SSD")
-# # Verify that the path exists
-# if os.path.exists(ssd_Path):
-#     files = os.listdir(ssd_Path)
-
-#     for file in files:  # currently printing all parent folders. 0 depth.
-#         print(file)
-#     print("\nAll files printed")
-# else:
-#     print(f"path {ssd_Path} does not exist.")
