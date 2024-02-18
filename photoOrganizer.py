36d0ea572d596e9f11b9778ca222153c0a79373f
Obie Carnathan <aikia16@uab.edu>
2024-02-17 23:42:46 -0600

    Code now successfully moves photos and videos to new folders. Next step is to organize the folder by creating sub folders that separate the files by date.

--- photoOrganizer.py
+++ photoOrganizer.py
@@ -38,50 +38,58 @@ video_extensions = [
     ".asf",
 ]
 
+new_location = "/Volumes/Sandisk SSD/"
 
-def create_and_move_folder(source_path, destination_folder):
+count = 0
+
+
+def create_and_move_file(file_path, destination_folder):
+    global count
     # Create a new folder
-    new_folder_path = os.path.join(destination_folder, "NewFolder")
-    os.makedirs(new_folder_path, exist_ok=True)
+    # new_folder_path = os.path.join(destination_folder, "NewFolder")
+    os.makedirs(destination_folder, exist_ok=True)
 
     # Move files from the source path to the new folder
-    for file_name in os.listdir(source_path):
-        file_path = os.path.join(source_path, file_name)
-        if os.path.isfile(file_path):
-            shutil.move(file_path, os.path.join(new_folder_path, file_name))
-            print(f"Moved {file_name} to {new_folder_path}")
+    file_path
+    file_name = file_path.split("/")[-1]
+    if os.path.isfile(file_path):
+        shutil.move(file_path, os.path.join(destination_folder, file_name))
+        print(f"Moved {file_name} to {destination_folder}")
+        count = count + 1
 
 
 def find_Photos_And_Videos(path):
     try:
         if os.path.exists(path):  # if the file path is an actual path
-            files = os.listdir(
-                path
-            )  # creates a list of files/folders in the current directory
-            count = 0  # used to keep track of the total files
+            # creates a list of files/folders in the current directory
+            files = os.listdir(path)
             for file in files:  # Loops through the list of files
-                file_path = os.path.join(
-                    path, file
-                )  # Creates a file path by joining the path with the current file name
+                # Creates a file path by joining the path with the current file name
+                file_path = os.path.join(path, file)
                 try:  # Checks to see if the current file is actually a folder
-                    if os.path.isdir(file_path):
+                    if os.path.isdir(file_path) and file_path.split("/")[-1] not in (
+                        "photos",
+                        "videos",
+                    ):
                         # recursively uses the current folder to print the files that it contains
                         find_Photos_And_Videos(file_path)
                     else:
+                        # gets file extension
                         file_extension = os.path.splitext(file)[1]
+                        # TODO: Create_and_move_file function needs work
                         if str(file_extension).lower() in (photo_extensions):
-                            print(f"{file} is a photo.")
+                            new_location_1 = new_location + "photos"
+                            create_and_move_file(file_path, new_location_1)
+                            # print(f"{file} is a photo.")
                         if str(file_extension).lower() in (video_extensions):
-                            print(f"{file} is a video.")
+                            new_location_2 = new_location + "videos"
+                            create_and_move_file(file_path, new_location_2)
+                            # print(f"{file} is a video.")
 
                 # Some files are restricted so we need this except case
                 except PermissionError as e:
                     # print(f"PermissionError: {e}. Skipping {file_path}")
                     continue
-
-            # print("\nAll files printed")
-            # print(count)
-
         else:
             print(f"path {path} does not exist.")
     except (
@@ -91,3 +99,5 @@ def find_Photos_And_Videos(path):
 
 
 find_Photos_And_Videos("/Volumes/Sandisk SSD")
+
+print(f"{count} files moved!")
