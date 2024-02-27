34848745ce84a0b54e01182be53f7328ef532912
Obie C <aikia16@uab.edu>
2024-02-27 08:17:12 -0600

    added references to os and shutil module in preparation for implementing folder organization. May need to redownload photos/videos from google because I've noticed that the meta data appears to have been reset during file movement. The original meta data is neccessary to organize files by date.

--- photoOrganizer.py
+++ photoOrganizer.py
@@ -1,9 +1,14 @@
 import os  # The os module provides a portable way of using operating system dependent functionality.
 import shutil
-
+#  References
+'''
+https://docs.python.org/3/library/os.html
+https://docs.python.org/3/library/shutil.html#module-shutilhttps://docs.python.org/3/library/shutil.html#module-shutil
+'''
 print(
     os.name
-)  # Prints posix. POSIX stands for Portable Operating System Interface. It's a set of standards that help software developers work across platforms. POSIX is based on the Unix operating system and is specified by the IEEE Computer Society.
+)  # Prints posix. POSIX stands for Portable Operating System Interface. It's a set of standards that help software developers work across platforms. 
+    # POSIX is based on the Unix operating system and is specified by the IEEE Computer Society.
 
 # First I need to figure out how to navigate through the folders and print all files
 
@@ -48,16 +53,19 @@ def create_and_move_file(file_path, destination_folder):
     # Create a new folder
     # new_folder_path = os.path.join(destination_folder, "NewFolder")
     os.makedirs(destination_folder, exist_ok=True)
-
     # Move files from the source path to the new folder
-    file_path
     file_name = file_path.split("/")[-1]
     if os.path.isfile(file_path):
         shutil.move(file_path, os.path.join(destination_folder, file_name))
         print(f"Moved {file_name} to {destination_folder}")
         count = count + 1
 
-
+'''
+This function recursively traverses a directory specified
+by the path argument, categorizing files into "photos" and "videos" based on their file extensions.
+It uses the helper function create_and_move_file to 
+It also handles PermissionErrors that may arise during the traversal.
+'''
 def find_Photos_And_Videos(path):
     try:
         if os.path.exists(path):  # if the file path is an actual path
@@ -66,12 +74,12 @@ def find_Photos_And_Videos(path):
             for file in files:  # Loops through the list of files
                 # Creates a file path by joining the path with the current file name
                 file_path = os.path.join(path, file)
-                try:  # Checks to see if the current file is actually a folder
+                try:  # Checks to see if the current file is actually a folder and the folder is not the photos or videos folder
                     if os.path.isdir(file_path) and file_path.split("/")[-1] not in (
                         "photos",
                         "videos",
                     ):
-                        # recursively uses the current folder to print the files that it contains
+                        # recursively uses the current folder to migrate the files that it contains
                         find_Photos_And_Videos(file_path)
                     else:
                         # gets file extension
@@ -98,6 +106,11 @@ def find_Photos_And_Videos(path):
         print(f"PermissionError: {e}. Skipping {path}")
 
 
-# find_Photos_And_Videos("/Volumes/Sandisk SSD")
+find_Photos_And_Videos("/Volumes/Sandisk SSD")
 
-# print(f"{count} files moved!")
+print(f"{count} files moved!")
+
+
+def create_Sub_Folders(folder_Name):
+    pass
+
